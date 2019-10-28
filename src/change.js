cf4730050baca0a5dbfd33973cceb5dedddab741
Keeran Sivaneri <KSivaneri@ucvts.tec.nj.us>
2019-10-28 10:23:12 -0400

    finished excercise 1

--- src/change.js
+++ src/change.js
@@ -1,14 +1,32 @@
 const readlineSync = require("readline-sync");
 
+// console.log("");
+
 const MIN = 0.01;
 const MAX = 0.99;
 
-const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
+const QUARTER_VALUE = 0.25;
+const DIME_VALUE = 0.1;
+const NICKEL_VALUE = 0.05
+
+let amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("Invalid.");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("Invalid.");
 } else {
-    // write your code for making change here
+    let q = Math.floor(amount / QUARTER_VALUE);
+    amount = amount % QUARTER_VALUE;
+
+    let d = Math.floor(amount / DIME_VALUE);
+    amount = amount % DIME_VALUE;
+
+    let n = Math.floor(amount / NICKEL_VALUE);
+    amount = amount % NICKEL_VALUE;
+
+    let p = Math.round(amount * 100);
+
+    console.log("");
+    console.log(q + " quarters, " + d + " dimes, " + n + " nickels, " + p + " pennies.");
 }

68d35b692cb9c6a9a386bcad18c591eba809e104
Keeran Sivaneri <KSivaneri@ucvts.tec.nj.us>
2019-10-28 10:50:18 -0400

    made adjustments to excercise 2

--- src/sign.js
+++ src/sign.js
@@ -2,6 +2,9 @@ const readlineSync = require("readline-sync");
 
 const number = readlineSync.question("\nEnter a number: ");
 
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+
 if (number == 0) {
     console.log("Zero.")
 } else if (number < 0) {
@@ -9,6 +12,10 @@ if (number == 0) {
 } else if (number > 0) {
     console.log("Positive.");
 } else if (Number.isNaN(number)) {
+    console.log("Invalid.");
+} else if (number < MIN || number > MAX) {
+    console.log("Invalid.");
+} else if (number % 1 != 0) {
     console.log("Invalid.");
 } else {
     console.log("Invalid.");

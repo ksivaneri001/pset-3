cd7e1e1cf444344f590fc5fd29d7f50a724c2008
Keeran Sivaneri <KSivaneri@ucvts.tec.nj.us>
2019-10-30 09:40:32 -0400

    fixed sign.js because it wasn't functioning properly

--- src/sign.js
+++ src/sign.js
@@ -5,18 +5,21 @@ const number = readlineSync.question("\nEnter a number: ");
 const MIN = Number.MIN_SAFE_INTEGER;
 const MAX = Number.MAX_SAFE_INTEGER;
 
-if (number == 0) {
-    console.log("Zero.")
-} else if (number < 0) {
-    console.log("Negative.");
-} else if (number > 0) {
-    console.log("Positive.");
-} else if (Number.isNaN(number)) {
-    console.log("Invalid.");
-} else if (number < MIN || number > MAX) {
-    console.log("Invalid.");
-} else if (number % 1 != 0) {
-    console.log("Invalid.");
-} else {
-    console.log("Invalid.");
+if (Number.isNaN(number)) {
+    console.log("\nInvalid.")
+}
+else if (number < MIN || number > MAX) {
+    console.log("\nInvalid.");
+}
+else if (number == 0) {
+    console.log("\nZero.");
+}
+else if (number > 0) {
+    console.log("\nPositive.");
+}
+else if (number < 0) {
+    console.log("\nNegative.");
+}
+else {
+    console.log("\nInvalid.");
 }

e8c2603c2bbd3fdd955cf8e8ad62661259de3972
Keeran Sivaneri <KSivaneri@ucvts.tec.nj.us>
2019-11-01 10:47:29 -0400

    finished excercise 4

--- src/sequence.js
+++ src/sequence.js
@@ -3,9 +3,11 @@ const readlineSync = require("readline-sync");
 const MIN = Number.MIN_SAFE_INTEGER;
 const MAX = Number.MAX_SAFE_INTEGER;
 
-const number1 = readlineSync.question("\nEnter a number: ");
-const number2 = readlineSync.question("\nEnter a number: ");
-const number3 = readlineSync.question("\nEnter a number: ");
+console.log("\nEnter three numbers.");
+
+const number1 = readlineSync.question("\n");
+const number2 = readlineSync.question("");
+const number3 = readlineSync.question("");
 
 if (Number.isNaN(number1) || Number.isNaN(number2) || Number.isNaN(number3)) {
     console.log("\nInvalid.");
@@ -20,13 +22,28 @@ else if (number3 < MIN || number3 > MAX) {
     console.log("\nInvalid.");
 }
 else {
-    if (number1 < number2 < number3) {
+    if (number1 < number2 && number2 < number3) {
         console.log("\nStrictly Increasing.");
     }
-    else if (number1 < number3 && number1 >= number2) {
-
+    else if (number1 < number2 && number2 == number3) {
+        console.log("\nIncreasing.");
     }
-    else if (number1 < number3 && number2 >= number3) {
-
+    else if (number1 == number2 && number2 < number3) {
+        console.log("\nIncreasing.");
+    }
+    else if (number1 == number2 && number2 == number3) {
+        console.log("\nEqual.");
+    }
+    else if (number1 > number2 && number2 > number3) {
+        console.log("\nStrictly Decreasing.");
+    }
+    else if (number1 > number2 && number2 == number3) {
+        console.log("\nDecreasing");
+    }
+    else if (number1 == number2 && number2 > number3) {
+        console.log("\nDecreasing");
+    }
+    else {
+        console.log("\nUnordered.");
     }
 }
